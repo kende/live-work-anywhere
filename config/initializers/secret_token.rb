a8ddc3e8a6f1ba239df1f79e75d84f5c004885ee
kende <kende.j@gmail.com>
2011-12-27 17:07:11 -0500

    Let's get it started in here...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Livework::Application.config.secret_token = '9f056765112627d54198c88b7857d4577d0a16cd07dfd3925af815216ae0fba742eaee413e12dec58a59183f097037e1e4d0fbb4ee6e963105bfd93a4a205f03'
